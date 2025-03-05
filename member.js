6d5ef7b90054296baf88de68067e16cc1cd26ea1
Ifran Laloe <ifran.laloe@navara.nl>
2025-03-05 14:47:26 +0000

    Add member.js

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 26,
+        profession: 'Software Developer',
+        skills: ['JavaScript', 'React', 'NodeJS']
+    };
+    return member;
+}
